a2cca871d743d81625d566d6a59a8e9006b529c3
samsom <samcoding2021@gmail.com>
2025-04-05 19:01:59 -0700

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -11,8 +11,8 @@ const nextConfig = {
     unoptimized: true,
   },
   // Replace 'portfolio' with your repository name if different
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio-2' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio-2/' : '',
   // Keep any experimental features you need
   experimental: {
     webpackBuildWorker: true,
